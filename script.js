e6c684e6627589026cc6ae69420cb83be2e54d66
CodingFactoryT <tim.baesler@gmail.com>
2023-01-11 00:31:06 +0100

    renamed some files

---
+++ script.js
@@ -0,0 +1,11 @@
+function aboutmeButton_Clicked(){
+    location.assign('./aboutme/index.html');
+}
+
+function contactButton_Clicked(){
+    location.assign('./contact/index.html');
+}
+
+function projectsButton_Clicked(){
+    location.assign('./projects/index.html');
+}
